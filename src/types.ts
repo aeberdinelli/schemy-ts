dc9e81d9cf656e1ada89f7d18346dd48c77231f7
Alan Berdinelli <alan.berdinelli@clinifyhealth.com>
2021-06-03 07:02:50 -0300

    Init Repo

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface SchemyPlugin {
+	beforeParse?: Function,
+	afterParse?: Function,
+	beforeValidate?: Function,
+	afterValidate?: Function,
+	getValidationErrors?: Function
+};
+
+export type SchemyPluginEvent = 'beforeParse' | 'afterParse' | 'beforeValidate' | 'afterValidate' | 'getValidationErrors';
+
+export interface SchemyProperties {
+	type?: String|Boolean|Function|Number|Date|SchemySchema,
+	required?: Boolean,
+	custom?: Function,
+	regex?: RegExp,
+	min?: Number,
+	max?: Number,
+	enum?: String[],
+	default?: any
+};
+
+export interface SchemySchema {
+	[key: string]: SchemyProperties
+};
+
+export interface SchemyOptions {
+	strict?: Boolean
+};
